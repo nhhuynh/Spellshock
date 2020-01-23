9e905340ad4c9c5f01595e081f3f82cdf38001dc
Nam Huynh <nhhuynh97@gmail.com>
2020-01-22 20:25:16 -0800

    v1.4

--- Spellshock/Assets/MyScripts/ARTapToPlaceScene.cs
+++ Spellshock/Assets/MyScripts/ARTapToPlaceScene.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceScene : MonoBehaviour
 {
+    public GameObject gameObjectToInstantiate;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        _arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount  > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+          return;
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                Debug.Log("object already spawned");
+            }
+        }
     }
 }
